05f5175a05ca7ea06fa0e9aee72fdd76efeb0703
Jannik Wibker <jannikwibker@gmail.com>
2020-03-14 23:47:46 +0100

    changed redflat.float.notify to use dunst instead of own notification thingy

--- float/notify.lua
+++ float/notify.lua
@@ -7,14 +7,11 @@
 
 -- Grab environment
 -----------------------------------------------------------------------------------------------------------------------
-local unpack = unpack or table.unpack
 local beautiful = require("beautiful")
-local wibox = require("wibox")
-local timer = require("gears.timer")
+
+local awful = require("awful")
 
 local redutil = require("redflat.util")
-local svgbox = require("redflat.gauge.svgbox")
-local progressbar = require("redflat.gauge.graph.bar")
 
 -- Initialize tables for module
 -----------------------------------------------------------------------------------------------------------------------
@@ -22,118 +19,52 @@ local notify = { last = {} }
 
 -- Generate default theme vars
 -----------------------------------------------------------------------------------------------------------------------
-local function default_style()
-	local style = {
-		geometry        = { width = 480, height = 48 }, -- height = 100
-		screen_gap      = 0,
-		set_position    = nil,
-		border_margin   = { 20, 20, 20, 20 },
-		elements_margin = { 20, 0, 10, 10 },
-		bar_width       = 8,
-		font            = "Helvetica Neue 12",
-		border_width    = 2,
+local function default_args()
+	local args = {
 		timeout         = 5,
 		icon            = nil,
-		progressbar     = {},
-		color           = { border = "#575757", icon = "#aaaaaa", wibox = "#202020" },
-		icon_size				= 24,
-		shape           = nil
+		urgency					= nil,
+		appname				  = "awesomewm",
+		hint						= nil
 	}
-	return redutil.table.merge(style, redutil.table.check(beautiful, "float.notify") or {})
+	return redutil.table.merge(args, redutil.table.check(beautiful, "float.notify") or {})
 end
 
 -- Initialize notify widget
 -----------------------------------------------------------------------------------------------------------------------
 function notify:init()
-
-	local style = default_style()
-	-- local icon = style.icon
-
-	self.style = style
-
-	-- Construct layouts
-	--------------------------------------------------------------------------------
-	local area = wibox.layout.align.horizontal()
-
-	local bar = progressbar(style.progressbar)
-	local image = svgbox()
-	local text = wibox.widget.textbox("100%")
-	text:set_align("center")
-	-- text:set_font(style.font)
-
-	local align_vertical = wibox.layout.align.vertical()
-	bar:set_forced_height(style.bar_width)
-
-	area:set_left(image)
-	area:set_middle(wibox.container.margin(align_vertical, unpack(style.elements_margin)))
-
-	-- Create floating wibox for notify widget
-	--------------------------------------------------------------------------------
-	self.wibox = wibox({
-		ontop        = true,
-		bg           = style.color.wibox,
-		border_width = style.border_width,
-		border_color = style.color.border,
-		shape        = style.shape
-	})
-
-	self.wibox:set_widget(wibox.container.margin(area, unpack(style.border_margin)))
-	self.wibox:geometry(style.geometry)
-
-	-- Set info function
-	--------------------------------------------------------------------------------
-	function self:set(args)
-		args = args or {}
-		align_vertical:reset()
-
-		if args.value then
-			bar:set_value(args.value)
-			align_vertical:set_top(text)
-			align_vertical:set_bottom(bar)
-		else
-			align_vertical:set_middle(text)
-		end
-
-		if args.text then
-			text:set_text(args.text)
-			text:set_font(args.font ~= nil and args.font or style.font)
-		end
-
-		image:set_image(args.icon ~= nil and args.icon or style.icon)
-		image:set_color(args.color or style.color.icon)
-	end
-
-	-- Set autohide timer
-	--------------------------------------------------------------------------------
-	self.hidetimer = timer({ timeout = style.timeout })
-	self.hidetimer:connect_signal("timeout", function() self:hide() end)
-
-	-- Signals setup
-	--------------------------------------------------------------------------------
-	self.wibox:connect_signal("mouse::enter", function() self:hide() end)
-end
-
--- Hide notify widget
------------------------------------------------------------------------------------------------------------------------
-function notify:hide()
-	self.wibox.visible = false
-	self.hidetimer:stop()
+	self.args = default_args()
 end
 
 -- Show notify widget
 -----------------------------------------------------------------------------------------------------------------------
 function notify:show(args)
-	if not self.wibox then self:init() end
-	self:set(args)
+	if not self.args then self:init() end
 
-	if not self.wibox.visible or mouse.screen.index ~= self.last.screen then
-		if self.style.set_position then self.style.set_position(self.wibox) end
-		redutil.placement.no_offscreen(self.wibox, self.style.screen_gap, mouse.screen.workarea)
-		self.wibox.visible = true
+	local all_args = redutil.table.merge(self.args, args)
+
+	local cmd = "dunstify "
+	if all_args.appname then
+		cmd = cmd .. "--appname='" .. all_args.appname .. "' "
+	end
+	if all_args.urgency then
+		cmd = cmd .. "--urgency='" .. all_args.urgency .. "' "
+	end
+	if all_args.icon then
+		cmd = cmd .. " --icon='" .. all_args.icon .."' "
+	end
+	if all_args.timeout then
+		cmd = cmd .. "--timeout=" .. (all_args.timeout * 1000) .. " "
+	end
+	if all_args.hint then
+		-- TODO: use an array instead?
+		--h, --hint=TYPE:NAME:VALUE        Specifies basic extra data to pass. Valid types are int, double, string and byte.
+		cmd = cmd .. "--hint='" .. all_args.hint .. "'"
 	end
 
-	self.last.screen = mouse.screen.index
-	self.hidetimer:again()
+	-- FOR DEBUGGING
+	-- awful.spawn("notify-send \"" .. cmd .. "' " .. all_args.text .. "'\"")
+	awful.spawn(cmd .. "' " .. all_args.text .. "'")
 end
 
 -- End
